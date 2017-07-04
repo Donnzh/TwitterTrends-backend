c7836d660bc4c8e399c8312a2a35c9c211b57291
Donn Zhao <z.donn@emondo.com.au>
2017-07-04 23:47:47 +1000

    add Access-Control-Allow-Origin

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const config = require('config');
 const bodyParser = require('body-parser');
 const winston = require('winston');
+const compress = require('compression');
 
 winston.info('Config', { config: JSON.stringify(config, undefined, 2) });
 winston.info('NODE_ENV', { NODE_ENV: process.env.NODE_ENV });
@@ -11,6 +12,14 @@ winston.remove(winston.transports.Console);
 winston.add(winston.transports.Console, config.get('logger.console'));
 
 const server = express();
+server.use(compress());
+
+server.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+});
+
 const allMiddlewares = [bodyParser.json()];
 const serverConfig = config.server;
 
